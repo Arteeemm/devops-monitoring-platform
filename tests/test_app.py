f5cc229360413d2f04c252f3d11754e67ca88747
Artyom <78357165+Arteeemm@users.noreply.github.com>
2026-02-09 22:58:48 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,22 @@
+# tests/test_app.py
+import sys
+from pathlib import Path
+
+# Получаем абсолютный путь к корню проекта
+project_root = Path(__file__).parent.parent
+sys.path.insert(0, str(project_root))
+
 from fastapi.testclient import TestClient
 from app.main import app
-  
 
 client = TestClient(app)
 
 
 def test_root():
-	response = client.get("/")
-	assert response.status_code == 200
+    response = client.get("/")
+    assert response.status_code == 200
 
 
 def test_metrics():
-	response = client.get("/metrics")
-	assert response.status_code == 200
+    response = client.get("/metrics")
+    assert response.status_code == 200
