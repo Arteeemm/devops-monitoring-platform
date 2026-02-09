6dcb08dc049a7c5d9a666931f453cbf244899f77
Artyom <78357165+Arteeemm@users.noreply.github.com>
2026-02-09 22:49:30 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ from prometheus_client import Gauge, generate_latest, CONTENT_TYPE_LATEST
 from fastapi.responses import Response
 import psutil
 
+
 app = FastAPI()
+
 
 # Метрики Prometheus
 cpu_gauge = Gauge('system_cpu_percent', 'CPU usage in percent')
 mem_gauge = Gauge('system_memory_percent', 'Memory usage in percent')
+
 
 @app.get("/system")
 def system_info():
@@ -18,13 +21,16 @@ def system_info():
         "memory_percent": mem
     }
 
+
 @app.get("/metrics")
 def metrics():
     # Получаем текущие значения
     cpu = psutil.cpu_percent()
     mem = psutil.virtual_memory().percent
+    
     # Обновляем метрики Prometheus
     cpu_gauge.set(cpu)
-    mem_gauge.set(mem) 
+    mem_gauge.set(mem)
+    
     # Возвращаем в формате Prometheus
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
