6e0e9001819153e474d8aa6fd146bbbf76d36d7d
Arteeemm <artyom.tis@yandex.ru>
2026-02-09 17:52:48 +0000

    Initial commit: monitoring platform with FastAPI, Prometheus, Grafana

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from prometheus_client import start_http_server, Gauge, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
+import psutil
+
+app = FastAPI()
+
+# Метрики Prometheus
+cpu_gauge = Gauge('system_cpu_percent', 'CPU usage in percent')
+mem_gauge = Gauge('system_memory_percent', 'Memory usage in percent')
+
+@app.get("/system")
+def system_info():
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    return {
+        "cpu_percent": cpu,
+        "memory_percent": mem
+    }
+
+@app.get("/metrics")
+def metrics():
+    # Обновляем значения
+    cpu_gauge.set(psutil.cpu_percent())
+    mem_gauge.set(psutil.virtual_memory().percent)
+    # Возвращаем в формате Prometheus
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
