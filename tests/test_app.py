2ac26e32bb44dff2cc25d943fe8e63f61606a1c4
Artyom <78357165+Arteeemm@users.noreply.github.com>
2026-02-09 23:03:53 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,19 @@
-# tests/test_app.py
-import sys
-from pathlib import Path
-
-# Получаем абсолютный путь к корню проекта
-project_root = Path(__file__).parent.parent
-sys.path.insert(0, str(project_root))
-
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
 
-def test_root():
-    response = client.get("/")
+def test_system():
+    response = client.get("/system")
     assert response.status_code == 200
+    data = response.json()
+    assert "cpu_percent" in data
+    assert "memory_percent" in data
 
 
 def test_metrics():
     response = client.get("/metrics")
     assert response.status_code == 200
+    # Дополнительная проверка для формата Prometheus
+    assert "text/plain" in response.headers["content-type"]
